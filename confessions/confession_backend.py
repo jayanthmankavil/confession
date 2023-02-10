33e0246cc0bbc084c4920337f6b5a091806f092a
mohd-fa <91002304+mohd-fa@users.noreply.github.com>
2023-02-10 23:14:57 +0530

    nc

--- confessions/confession_backend.py
+++ confessions/confession_backend.py
@@ -7,24 +7,28 @@ app = Flask(__name__)
 def get_db():
     conn = sqlite3.connect('confessions.db')
     conn.execute('''CREATE TABLE IF NOT EXISTS confessions
-                     (id INTEGER PRIMARY KEY AUTOINCREMENT, from TEXT,to TEXT, confession TEXT)''')
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT, sender TEXT, reciever TEXT, confession TEXT)''')
     return conn
 
 @app.route('/')
 def index():
     conn = get_db()
     confessions = conn.execute("SELECT * FROM confessions").fetchall()
+    conn.close()
     return render_template('viewconfession.html', confessions=confessions)
 
 @app.route('/confessions', methods=['POST'])
 def submit_confession():
-    f = request.form.get('from')
-    to = request.form.get('to')
+    fr = request.form.get('sender')
+    to = request.form.get('reciever')
     confession = request.form.get('confession')
 
     conn = get_db()
-    conn.execute("INSERT INTO confessions (from, to, confession) VALUES (?, ?, ?)", (f, to, confession))
+    conn.execute("INSERT INTO confessions (sender, reciever, confession) VALUES (?, ?, ?)", (fr, to, confession))
     conn.commit()
     conn.close()
 
     return 'Confession submitted successfully!'
+
+if __name__ == '__main__':
+    app.run(port=8000)
