11b97362bf2a7d5c8a9d373d64d212704263b691
Mohammed F A <91002304+mohd-fa@users.noreply.github.com>
2023-02-10 22:16:24 +0530

    Update confession_backend.py

--- confessions/confession_backend.py
+++ confessions/confession_backend.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+
+from flask import Flask, request, render_template
 import sqlite3
 
 app = Flask(__name__)
@@ -6,20 +7,24 @@ app = Flask(__name__)
 def get_db():
     conn = sqlite3.connect('confessions.db')
     conn.execute('''CREATE TABLE IF NOT EXISTS confessions
-                     (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, confession TEXT)''')
+                     (id INTEGER PRIMARY KEY AUTOINCREMENT, from TEXT,to TEXT, confession TEXT)''')
     return conn
+
+@app.route('/')
+def index():
+    conn = get_db()
+    confessions = conn.execute("SELECT * FROM confessions").fetchall()
+    return render_template('viewconfession.html', confessions=confessions)
 
 @app.route('/confessions', methods=['POST'])
 def submit_confession():
-    name = request.form.get('name')
+    f = request.form.get('from')
+    to = request.form.get('to')
     confession = request.form.get('confession')
-    
+
     conn = get_db()
-    conn.execute("INSERT INTO confessions (name, confession) VALUES (?, ?)", (name, confession))
+    conn.execute("INSERT INTO confessions (from, to, confession) VALUES (?, ?, ?)", (f, to, confession))
     conn.commit()
     conn.close()
-    
-    return 'Confession submitted successfully!'
 
-if __name__ == '__main__':
-    app.run(debug=False)
+    return 'Confession submitted successfully!'
